8a9bc288bd3866aedd0d9173395b406e8f190929
Christina Dittmann <dittmann.christina@gmx.de>
2023-12-14 16:09:35 +0100

    changed db

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 # settings.py
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
